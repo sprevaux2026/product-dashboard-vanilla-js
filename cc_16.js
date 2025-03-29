a47cdc8fa8235c9a85eef692d1e75b992a34c2ae
sprevaux2026 <stevenprevaux@gmail.com>
2025-03-29 07:17:50 -0400

    Add reusable handleError function

--- cc_16.js
+++ cc_16.js
@@ -68,3 +68,8 @@ function displayProducts(products) {
     });
   }
   
+  // Task 5: Reusable Error Handler
+function handleError(error) {
+    console.error('An error occurred:', error.message);
+  }
+  

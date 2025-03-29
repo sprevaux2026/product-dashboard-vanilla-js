3edc24b981c9719898d8711b2b81f328f9010830
sprevaux2026 <stevenprevaux@gmail.com>
2025-03-29 07:15:08 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -17,4 +17,19 @@ function fetchProductsThen() {
         console.error('Error fetching products with .then():', error);
       });
   }
+
+  // Task 3: Fetch Products with async/await
+async function fetchProductsAsync() {
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const products = await response.json();
+      //Display products in the DOM
+      displayProducts(products);
+    } catch (error) {
+      handleError(error);
+    }
+  }
   
